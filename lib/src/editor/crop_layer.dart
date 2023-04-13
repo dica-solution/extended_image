acc0ed7301664ad89ac5357f0a86f5fcff76a27b
Maxwell Tang <tuyentangtruong@gmail.com>
2023-04-13 10:33:54 +0700

    [fix] negative width

--- lib/src/editor/crop_layer.dart
+++ lib/src/editor/crop_layer.dart
@@ -120,8 +120,8 @@ class ExtendedImageCropLayerState extends State<ExtendedImageCropLayer>
             top: cropRect!.top + gWidth,
             left: cropRect!.left + gWidth,
             child: Container(
-              width: cropRect!.width - 2 * gWidth,
-              height: cropRect!.height - 2 * gWidth,
+              width: max(cropRect!.width - 2 * gWidth, 0),
+              height: max(cropRect!.height - 2 * gWidth, 0),
               child: GestureDetector(
                   behavior: HitTestBehavior.translucent,
                   onPanUpdate: (DragUpdateDetails details) {
@@ -210,7 +210,7 @@ class ExtendedImageCropLayerState extends State<ExtendedImageCropLayer>
             left: cropRect!.left + gWidth,
             child: Container(
               height: gWidth * 2,
-              width: max(cropRect!.width - gWidth * 2, gWidth * 2),
+              width: max(cropRect!.width - gWidth * 2, 0),
               child: GestureDetector(
                 behavior: HitTestBehavior.translucent,
                 onVerticalDragUpdate: (DragUpdateDetails details) {
@@ -227,7 +227,7 @@ class ExtendedImageCropLayerState extends State<ExtendedImageCropLayer>
             top: cropRect!.top + gWidth,
             left: cropRect!.left - gWidth,
             child: Container(
-              height: max(cropRect!.height - gWidth * 2, gWidth * 2),
+              height: max(cropRect!.height - gWidth * 2, 0),
               width: gWidth * 2,
               child: GestureDetector(
                 behavior: HitTestBehavior.translucent,
@@ -246,7 +246,7 @@ class ExtendedImageCropLayerState extends State<ExtendedImageCropLayer>
             left: cropRect!.left + gWidth,
             child: Container(
               height: gWidth * 2,
-              width: max(cropRect!.width - gWidth * 2, gWidth * 2),
+              width: max(cropRect!.width - gWidth * 2, 0),
               child: GestureDetector(
                 behavior: HitTestBehavior.translucent,
                 onVerticalDragUpdate: (DragUpdateDetails details) {
@@ -263,7 +263,7 @@ class ExtendedImageCropLayerState extends State<ExtendedImageCropLayer>
             top: cropRect!.top + gWidth,
             left: cropRect!.right - gWidth,
             child: Container(
-              height: max(cropRect!.height - gWidth * 2, gWidth * 2),
+              height: max(cropRect!.height - gWidth * 2, 0),
               width: gWidth * 2,
               child: GestureDetector(
                 behavior: HitTestBehavior.translucent,
